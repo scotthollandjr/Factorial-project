5bcb5b2190879a609ca128fcbccfd182f9797fb7
Scott Holland <scotthollandjr@gmail.com>
2016-01-17 20:45:11 -0800

    Add user functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-var factorIt = function(entryNum) {
+function factorIt(entryNum) {
   if ( entryNum < 0 ) {
-    // alert("Please enter a valid number");
     return -1;
   } else if ( entryNum === 0 ) {
     return 1;
   } else if ( entryNum > 0 ) {
     return (entryNum * factorIt(entryNum - 1));
-  }
+  };
 };
+
+$(document).ready(function() {
+  $("form#factorial").submit(function(event) {
+    var entryNum = parseInt($("input#entry").val());
+
+    var factorNum = factorIt(entryNum);
+    alert(factorNum);
+
+    event.preventDefault();
+  });
+});
