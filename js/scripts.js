f8bd645980e5f56a6bc1f384408d9af1be42bd50
Scott Holland <scotthollandjr@gmail.com>
2016-01-17 19:59:49 -0800

    Add 0 functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var entryNum;
-
 var factorIt = function(entryNum) {
   if ( entryNum < 0 ) {
-    alert("Please enter a valid number");
-    return true;
+    // alert("Please enter a valid number");
+    return -1;
+  } else if ( entryNum === 0 ) {
+    return 1;
   }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,9 @@
 describe('factorIt', function() {
-  it("numbers less than 0 should return as invalid", function() {
-    expect(factorIt(-1)).to.equal(true);
+  it("numbers less than 0 should return as -1", function() {
+    expect(factorIt(-1)).to.equal(-1);
+  });
+
+  it("an entry of zero (0) should return 1", function() {
+    expect(factorIt(0)).to.equal(1);
   });
 });
